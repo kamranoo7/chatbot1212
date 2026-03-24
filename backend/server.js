fdcf5ec5aaac8cdd01b5073599acc32ac6989178
Kamran Khan <kamrankhan@Kamrans-MacBook-Air.local>
2026-03-24 18:01:20 +0530

    testing112

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(cors({
 
 /* ================= MIDDLEWARE ================= */
 app.use(express.json());
-app.use(cors());
+
 /* ================= ROUTES ================= */
 app.use("/api", require("./routes/aiRoutes"));
 
